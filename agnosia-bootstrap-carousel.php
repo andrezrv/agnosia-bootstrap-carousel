c9fe76d44a65321b0e1c3ffe83bb72410555091b
andrezrv <andrezrv@gmail.com>
2014-04-11 01:26:32 -0300

    Update plugin version.

--- agnosia-bootstrap-carousel.php
+++ agnosia-bootstrap-carousel.php
@@ -12,7 +12,7 @@
  * work.
  *
  * @package   Agnosia_Bootstrap_Carousel
- * @version   1.0
+ * @version   1.1
  * @author    Andrés Villarreal <andrezrv@gmail.com>
  * @license   GPL-2.0
  * @link      http://github.com/andrezrv/agnosia-bootstrap-carousel
@@ -24,7 +24,7 @@
  * Description: Hooks the <code>[gallery]</code> shortcode with attribute <code>type="carousel"</code> in order to show a <a href="http://getbootstrap.com/javascript/#carousel" target="_blank">Bootstrap Carousel</a> based on the selected images and their titles and descriptions. <strong>Very important:</strong> this plugin assumes either your theme includes the necessary Bootstrap Javascript and CSS files to display the carousel properly, or that you have included the files on your own. It will not include the files for you, so if they are not present, the carousel will not work.
  * Author: Andr&eacute;s Villarreal, AuSoft
  * Author URI: http://aufieroinformatica.com/wordpress/
- * Version: 1.0
+ * Version: 1.1
  */
 // Load Agnosia Bootstrap Carousel class.
 require( dirname( __FILE__ ) . '/agnosia-bootstrap-carousel.class.php' );
